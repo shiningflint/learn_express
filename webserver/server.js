fda80a6f7221b6da502d140c952fc4b81a38a94c
Adam Christopher <adam.keristoper@gmail.com>
2018-05-28 23:23:58 +0900

    Add JSON return

--- webserver/server.js
+++ webserver/server.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('<h2>Hello world!</h2><p><a href="/json">Go to JSON example</a></p>')
+})
+
+app.get('/json', (req, res) => {
+  res.send({
+    text: 'returning JSON here',
+    bananas: 2,
+    person: {
+      first_name: 'Bambang',
+      last_name: 'Santoso'
+    }
+  })
 })
 
 app.listen(8080)
